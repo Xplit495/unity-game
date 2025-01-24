a5a36ac7e509115d2a889dc1bec6cd5595303ad2
Xplit495 <carrolaquentin.pro@gmail.com>
2025-01-24 11:45:17 +0100

    Movement and following camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,3 @@
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,16 +9,35 @@ public class Player : MonoBehaviour
     public LayerMask groundLayer;
     public Transform groundCheck;
     float horizontal;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void FixedUpdate()
     {
-        
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(InputAction.CallbackContext context)
     {
-        
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+    
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+    
+    public void SuperJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce + 2);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
     }
 }
